8a141bc0be3ba707fca735e9ae2733fbce69405e
Kate Lawrence <klawren@smsh11dsu-srcf-d15-36.scg.stanford.edu>
2024-12-19 10:25:59 -0800

    update ABC, update subsampling to constant pcs

--- run_snakemake_subsampling.sh
+++ run_snakemake_subsampling.sh
@@ -4,5 +4,5 @@
 for i in 200 250 300 350 400 450
 do
     echo "Running command for $i:"
-    snakemake --snakefile workflow/Snakefile_subsampling --configfile config/subsampling/${i}_subsample.yaml --profile config/slurm_scg
+    snakemake --snakefile workflow/Snakefile_subsampling --configfile config/subsampling_constant_pcs/${i}_subsample.yaml --profile config/slurm_scg
 done
